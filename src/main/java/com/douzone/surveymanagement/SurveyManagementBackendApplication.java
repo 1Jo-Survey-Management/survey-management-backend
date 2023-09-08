d714f8f3deaa8266ea596ac17f7f69d299ecd368
gwanii <lung4536@gmail.com>
2023-09-08 14:07:58 +0900

    first commit

---
+++ src/main/java/com/douzone/surveymanagement/SurveyManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.douzone.surveymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyManagementBackendApplication.class, args);
+	}
+
+}
