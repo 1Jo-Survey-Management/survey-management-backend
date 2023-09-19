3d03ae2bb431c58889e4974db8693e62631438ca
gwanii <lung4536@gmail.com>
2023-09-19 16:52:21 +0900

    [feat]: 설문 등록을 위한 API 작성 #6

--- src/main/java/com/douzone/surveymanagement/survey/controller/CommandSurveyController.java
+++ src/main/java/com/douzone/surveymanagement/survey/controller/CommandSurveyController.java
@@ -1,5 +1,15 @@
 package com.douzone.surveymanagement.survey.controller;
 
+import com.douzone.surveymanagement.common.response.CommonResponse;
+import com.douzone.surveymanagement.survey.dto.request.SurveyCreateDto;
+import com.douzone.surveymanagement.survey.service.CommandSurveyService;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,5 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
  * @since : 1.0
  **/
 @RestController
+@RequestMapping("/v1/survey")
+@RequiredArgsConstructor
 public class CommandSurveyController {
+
+    private final CommandSurveyService commandSurveyService;
+
+    /**
+     * 설문을 등록하는 메서드 입니다.
+     *
+     * @param surveyCreateDto 설문에 대한 모든 정보를 담고있는 dto 입니다.
+     * @return Http Status: 201
+     * @author : 강명관
+     */
+    @PostMapping("/register")
+    public ResponseEntity<CommonResponse<String>> surveyRegister(
+        @Valid @RequestBody SurveyCreateDto surveyCreateDto
+    ) {
+        commandSurveyService.insertSurvey(surveyCreateDto);
+        return new ResponseEntity<>(CommonResponse.success(), HttpStatus.CREATED);
+    }
 }
