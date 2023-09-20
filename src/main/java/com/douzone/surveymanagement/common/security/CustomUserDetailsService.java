1fa6a5516f4cf9c679e3624c60a5d10e6010084a
DESKTOP-1IRTIK9\금정산2_PC22 <86181292+kl204@users.noreply.github.com>
2023-09-20 20:35:20 +0900

    [fix] 오류 수정 #3

--- src/main/java/com/douzone/surveymanagement/common/security/CustomUserDetailsService.java
+++ src/main/java/com/douzone/surveymanagement/common/security/CustomUserDetailsService.java
@@ -1,30 +1,30 @@
-package com.douzone.surveymanagement.common.security;
-import com.douzone.surveymanagement.user.mapper.UserMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CustomUserDetailsService implements UserDetailsService {
-
-    @Autowired
-    private UserMapper userMapper;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // MyBatis를 사용하여 사용자 정보를 데이터베이스에서 조회
-        User user = userMapper.findByUsername(username);
-        if (user == null) {
-            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username);
-        }
-        // UserDetails로 사용자 정보 매핑
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                user.getAuthorities()
-        );
-    }
-}
+//package com.douzone.surveymanagement.common.security;
+//import com.douzone.surveymanagement.user.mapper.UserMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class CustomUserDetailsService implements UserDetailsService {
+//
+//    @Autowired
+//    private UserMapper userMapper;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        // MyBatis를 사용하여 사용자 정보를 데이터베이스에서 조회
+//        User user = userMapper.findByUsername(username);
+//        if (user == null) {
+//            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username);
+//        }
+//        // UserDetails로 사용자 정보 매핑
+//        return new org.springframework.security.core.userdetails.User(
+//                user.getUsername(),
+//                user.getPassword(),
+//                user.getAuthorities()
+//        );
+//    }
+//}
