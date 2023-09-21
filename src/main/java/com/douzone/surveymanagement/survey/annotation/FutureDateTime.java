be8fab30a0ca9bccc3a3f13cfe26bbe2a989db17
gwanii <lung4536@gmail.com>
2023-09-21 15:21:40 +0900

    [refactor] : FuterDateTime Custom Validation Annotation Refactor commit #6

--- src/main/java/com/douzone/surveymanagement/survey/annotation/FutureDateTime.java
+++ src/main/java/com/douzone/surveymanagement/survey/annotation/FutureDateTime.java
@@ -6,6 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import javax.validation.Constraint;
+import javax.validation.Payload;
 
 /**
  * 입력된 시간이 현재 시간보다 이후인지 검사하기 위한 애너테이션입니다.
@@ -18,4 +19,6 @@ import javax.validation.Constraint;
 @Constraint(validatedBy = FutureDateTimeValidator.class)
 public @interface FutureDateTime {
     String message() default "입력된 날짜 및 시간은 미래 시간이어야 합니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
