8320880dd99f436e633184f62b6320ac0f2614bd
gwanii <lung4536@gmail.com>
2023-10-24 10:29:35 +0900

    [feat]: 이미지를 보여주기 위한 Controller && Util 클래스 추가 #21

---
+++ src/main/java/com/douzone/surveymanagement/common/controller/ImageController.java
@@ -0,0 +1,48 @@
+package com.douzone.surveymanagement.common.controller;
+
+import com.douzone.surveymanagement.common.utils.ImageUtil;
+import com.douzone.surveymanagement.survey.service.QuerySurveyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Some description here.
+ *
+ * @author : 강명관
+ * @since : 1.0
+ **/
+
+@Slf4j
+@RestController
+@RequestMapping("/api/images")
+@RequiredArgsConstructor
+public class ImageController {
+
+    private final QuerySurveyService querySurveyService;
+
+    /**
+     * 설문에 대한 이미지를 나타내는 컨트롤러 입니다.
+     *
+     * @param surveyNo 설문 번호
+     * @return 설문에 대한 이미지를 byte[]로 반환 합니다.
+     * @author : 강명관
+     */
+    @GetMapping("/surveys/{surveyNo}")
+    public ResponseEntity<byte[]> imageDisplay(@PathVariable(value = "surveyNo") long surveyNo) {
+
+        String surveyImagePath = querySurveyService.findSurveyImageBySurveyNo(surveyNo);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(ImageUtil.getExtensionMediaTypeByFileName(surveyImagePath));
+        return new ResponseEntity<>(ImageUtil.getImageByteArray(surveyImagePath), headers, HttpStatus.OK);
+    }
+
+
+}
