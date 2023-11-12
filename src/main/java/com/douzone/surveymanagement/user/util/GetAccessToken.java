e828524f8ee61ee1f21a0067b91783d0ef3b4bc7
PUO <86181292+kl204@users.noreply.github.com>
2023-11-12 19:28:57 +0900

    [fix] 자동 로그인 로직 수정 #40

--- src/main/java/com/douzone/surveymanagement/user/util/GetAccessToken.java
+++ src/main/java/com/douzone/surveymanagement/user/util/GetAccessToken.java
@@ -16,32 +16,36 @@ import java.util.Map;
 @Slf4j
 public class GetAccessToken {
 
-    public static Map<String, String> getToken(String AccessUrl) {
+//    static {
+//        // TLS 버전 설정
+//        System.setProperty("https.protocols", "TLSv1.2");
+//    }
+
+    public static Map<String, String> getToken(String accessUrl) {
 
         StringBuilder response = new StringBuilder();
 
         try {
-            URL url = new URL(AccessUrl);
+            URL url = new URL(accessUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
             int responseCode = connection.getResponseCode();
 
             if (responseCode == HttpURLConnection.HTTP_OK) {
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                    String inputLine;
 
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String inputLine;
-
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
                 }
-                in.close();
-
             } else {
-                log.error("HTTP Request Failed with Error Code: " + responseCode);
+                log.error("HTTP Request Failed with Error Code: {}", responseCode);
             }
 
         } catch (IOException e) {
+            log.error("IOException occurred while getting the access token: {}", e.getMessage());
             e.printStackTrace();
         }
 
@@ -50,19 +54,20 @@ public class GetAccessToken {
         return responseAccessCode;
     }
 
-
     public static Map<String, String> parseJsonResponse(String jsonResponse) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            // JSON 문자열을 Map으로 변환
-            Map<String, String> responseMap = objectMapper.readValue(jsonResponse, new TypeReference<>() {
-            });
-            return responseMap;
+            if (jsonResponse != null && !jsonResponse.isEmpty()) {
+                // JSON 문자열이 비어 있지 않은 경우에만 변환
+                return objectMapper.readValue(jsonResponse, new TypeReference<>() {});
+            } else {
+                log.warn("Empty or null JSON response. Returning an empty map.");
+                return Collections.emptyMap();
+            }
         } catch (IOException e) {
+            log.error("Error occurred while parsing JSON response: {}", e.getMessage());
             e.printStackTrace();
             return Collections.emptyMap(); // 예외 발생 시 빈 Map 반환 또는 예외 처리
         }
-
-
     }
 }
