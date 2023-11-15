73f9468422756cf271401e42639af95078434393
gwanii <lung4536@gmail.com>
2023-11-15 15:06:14 +0900

    [fix]: Dependency Injection 이 잘못되었다고 의심되는 파일 주석 && 수정 - 03

--- src/main/java/com/douzone/surveymanagement/mysurvey/service/impl/MySurveyServiceImpl.java
+++ src/main/java/com/douzone/surveymanagement/mysurvey/service/impl/MySurveyServiceImpl.java
@@ -1,36 +1,36 @@
-package com.douzone.surveymanagement.mysurvey.service.impl;
-
-import com.douzone.surveymanagement.mysurvey.dto.request.MySurveyDTO;
-import com.douzone.surveymanagement.mysurvey.mapper.MySurveyMapper;
-import com.douzone.surveymanagement.mysurvey.service.MySurveyService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-@Slf4j
-@Service
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class MySurveyServiceImpl implements MySurveyService {
-    private final MySurveyMapper mySurveyMapper;
-
-    @Override
-    public List<MySurveyDTO> selectMySurveysWithSorting(long userNo) {
-        return mySurveyMapper.selectMySurveysWithSorting(userNo);
-    }
-
-    @Override
-    public List<MySurveyDTO> selectMyParticipatedSurveys(long userNo) {
-
-        return mySurveyMapper.selectMyParticipatedSurveys(userNo);
-    }
-
-    @Override
-    @Transactional
-    public boolean deleteMySurveyInProgress(MySurveyDTO mySurveyDTO) {
-        return mySurveyDTO.getSurveyStatusNo() == 1 && mySurveyMapper.updateMySurveysInProgress(mySurveyDTO) == 1;
-    }
-}
+//package com.douzone.surveymanagement.mysurvey.service.impl;
+//
+//import com.douzone.surveymanagement.mysurvey.dto.request.MySurveyDTO;
+//import com.douzone.surveymanagement.mysurvey.mapper.MySurveyMapper;
+//import com.douzone.surveymanagement.mysurvey.service.MySurveyService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.List;
+//
+//@Slf4j
+//@Service
+//@RequiredArgsConstructor
+//@Transactional(readOnly = true)
+//public class MySurveyServiceImpl implements MySurveyService {
+//    private final MySurveyMapper mySurveyMapper;
+//
+//    @Override
+//    public List<MySurveyDTO> selectMySurveysWithSorting(long userNo) {
+//        return mySurveyMapper.selectMySurveysWithSorting(userNo);
+//    }
+//
+//    @Override
+//    public List<MySurveyDTO> selectMyParticipatedSurveys(long userNo) {
+//
+//        return mySurveyMapper.selectMyParticipatedSurveys(userNo);
+//    }
+//
+//    @Override
+//    @Transactional
+//    public boolean deleteMySurveyInProgress(MySurveyDTO mySurveyDTO) {
+//        return mySurveyDTO.getSurveyStatusNo() == 1 && mySurveyMapper.updateMySurveysInProgress(mySurveyDTO) == 1;
+//    }
+//}
