7bfda136cef7c13b8085501060ff43f046feff54
PUO <86181292+kl204@users.noreply.github.com>
2023-11-16 13:09:04 +0900

    [test] 빈생성 테스트9 #65
    잘못된 빈생성 예

--- src/main/java/com/douzone/surveymanagement/user/service/CustomAuthenticationProvider.java
+++ src/main/java/com/douzone/surveymanagement/user/service/CustomAuthenticationProvider.java
@@ -30,7 +30,8 @@ import com.douzone.surveymanagement.user.util.NTPTimeFetcher;
 @AllArgsConstructor
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
-    private final NaverClientProperties naverClientProperties;
+//    private final NaverClientProperties naverClientProperties;
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
     UserService userService;
 
@@ -46,12 +47,12 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
             // CustomAuthentication 객체가 아닌 객체가 접근시 접근 불가.
             if ((authentication instanceof CustomAuthenticationToken)) {
 //------------------------- 토큰 갱신 api 요청 위한 정보들 모음 -------------------------------------------------
-//                ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("naver");
-//                String clientId = clientRegistration.getClientId();
-//                String clientSecret = clientRegistration.getClientSecret();
+                ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("naver");
+                String clientId = clientRegistration.getClientId();
+                String clientSecret = clientRegistration.getClientSecret();
 
-                String clientId = naverClientProperties.getClientId();
-                String clientSecret = naverClientProperties.getClientSecret();
+//                String clientId = naverClientProperties.getClientId();
+//                String clientSecret = naverClientProperties.getClientSecret();
                 CustomAuthenticationToken customToken = (CustomAuthenticationToken) authentication;
                 String oldAccessToken = customToken.getCustomToken();
 
