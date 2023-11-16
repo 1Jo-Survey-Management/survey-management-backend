7244b2be9a59bb8a8644e8c91cfc54163dcb3138
PUO <86181292+kl204@users.noreply.github.com>
2023-11-16 10:56:54 +0900

    [test] 빈생성 테스트 #65

--- src/main/java/com/douzone/surveymanagement/common/security/SecurityConfig.java
+++ src/main/java/com/douzone/surveymanagement/common/security/SecurityConfig.java
@@ -1,14 +1,10 @@
 package com.douzone.surveymanagement.common.security;
 
 import com.douzone.surveymanagement.user.filter.CustomOAuth2Filter;
-import com.douzone.surveymanagement.user.service.CustomAuthenticationProvider;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -42,6 +38,7 @@ public class SecurityConfig {
         filter.setAuthenticationManager(authenticationManager);
         return filter;
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
