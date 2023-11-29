9b2b2c4ebc64c619f000a772c31ab52186b6a59d
gwanii <lung4536@gmail.com>
2023-11-29 16:30:08 +0900

    [refactor]: 설문 참여 로직 변경 #111

--- src/main/java/com/douzone/surveymanagement/surveyattend/controller/SurveyAttendController.java
+++ src/main/java/com/douzone/surveymanagement/surveyattend/controller/SurveyAttendController.java
@@ -1,16 +1,20 @@
 package com.douzone.surveymanagement.surveyattend.controller;
 
+import com.douzone.surveymanagement.common.exception.BadRequestException;
 import com.douzone.surveymanagement.common.response.CommonResponse;
 import com.douzone.surveymanagement.common.response.ErrorResponse;
+import com.douzone.surveymanagement.survey.service.QuerySurveyService;
 import com.douzone.surveymanagement.surveyattend.dto.request.SurveyAttendDTO;
 import com.douzone.surveymanagement.surveyattend.dto.request.SurveyAttendSubmitDTO;
 import com.douzone.surveymanagement.surveyattend.exception.SurveyAttendException;
-import com.douzone.surveymanagement.surveyattend.service.impl.SurveyAttendServiceImpl;
+import com.douzone.surveymanagement.surveyattend.service.SurveyAttendService;
+import com.douzone.surveymanagement.user.util.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -28,7 +32,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SurveyAttendController {
 
-    private final SurveyAttendServiceImpl surveyAttendServiceImpl;
+    private final SurveyAttendService surveyAttendService;
+
+    private final QuerySurveyService querySurveyService;
 
     /**
      * 설문에 참여하기 위한 데이터를 가져옵니다.
@@ -36,9 +42,23 @@ public class SurveyAttendController {
      * @return 설문 데이터 목록을 담은 ResponseEntity
      */
     @Operation(summary = "설문 데이터 가져오기", description = "설문 참여를 위한 데이터를 조회합니다.")
-    @GetMapping("/survey-data")
-    public ResponseEntity<CommonResponse> getSurveyData() {
-        List<SurveyAttendDTO> surveyData = surveyAttendServiceImpl.selectSurveyAttendData();
+    @GetMapping("/survey-data/{surveyNo}")
+    public ResponseEntity<CommonResponse> getSurveyData(
+        @PathVariable("surveyNo") long surveyNo,
+        @AuthenticationPrincipal CustomUserDetails customUserDetails
+        ) {
+
+        boolean isSurveyCreatedByUser = querySurveyService.isSurveyCreatedByUser(
+            customUserDetails.getUserNo(),
+            surveyNo
+        );
+
+        if (isSurveyCreatedByUser) {
+            throw new BadRequestException("설문의 작성자는 본인 설문을 참여할 수 없습니다.");
+        }
+
+        List<SurveyAttendDTO> surveyData = surveyAttendService.selectSurveyAttendData(surveyNo);
+
         return ResponseEntity
             .ok()
             .body(CommonResponse.successOf(surveyData));
@@ -61,7 +81,7 @@ public class SurveyAttendController {
     ) {
 
         try {
-            surveyAttendServiceImpl.saveSurveyAndAnswers(surveyAttendSubmitDTOList);
+            surveyAttendService.saveSurveyAndAnswers(surveyAttendSubmitDTOList);
             return ResponseEntity.ok().body(CommonResponse.success());
         } catch (SurveyAttendException e) {
             log.error("설문 응답 저장 중 오류 발생: {}", e.getMessage());
@@ -84,7 +104,7 @@ public class SurveyAttendController {
     @Operation(summary = "설문 마감일 조회하기", description = "설문 번호에 따른 마감일을 조회합니다.")
     public ResponseEntity<CommonResponse> getSurveyClosingTime(@PathVariable long surveyNo) {
         try {
-            LocalDateTime closingTime = surveyAttendServiceImpl.getSurveyClosingTime(surveyNo);
+            LocalDateTime closingTime = surveyAttendService.getSurveyClosingTime(surveyNo);
             return ResponseEntity.ok().body(CommonResponse.successOf(closingTime));
         } catch (Exception e) {
             log.error("마감일 정보 조회 중 오류 발생: {}", e.getMessage());
